d30128719a75d50909ea0817c9ea8cc4615494ad
Alexey <alexander170b@gmail.com>
2022-07-07 20:31:45 +0300

    подготовительные работы

---
+++ src/component/users.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import api from "../api";
+
+const Users = () => {
+  // console.log(api.users.fetchAll());
+  // return <h1>Users</h1>;
+
+  // const [users, setUsers] = useState(api.users.fetchAll());
+  // const hadleDelete = (userId) => {};
+  // const renderPrice = (number) => {};
+  return <table class="table table-dark table-hover">...</table>;
+};
+
+export default Users;
