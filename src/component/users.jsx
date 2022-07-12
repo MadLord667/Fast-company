cde183ab7bf4812a2d8a129076e2c4eaab34f430
Alexey <alexander170b@gmail.com>
2022-07-12 20:28:39 +0300

    finish

--- src/component/users.jsx
+++ src/component/users.jsx
@@ -1,14 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../api";
 
 const Users = () => {
-  // console.log(api.users.fetchAll());
-  // return <h1>Users</h1>;
+  const [users, setUsers] = useState(api.users.fetchAll());
 
-  // const [users, setUsers] = useState(api.users.fetchAll());
-  // const hadleDelete = (userId) => {};
-  // const renderPrice = (number) => {};
-  return <table class="table table-dark table-hover">...</table>;
+  const handleDelete = (userId) => {
+    // console.log(userId);
+    setUsers(users.filter((user) => user._id !== userId));
+  };
+
+  const renderPhrse = (number) => {
+    if (number === 2 || number === 3 || number === 4)
+      return " человека тусанут";
+    return " человек тусанёт";
+  };
+
+  const formatHeaderPhrase = () => {
+    let classes = "badge ";
+    return (classes += users.length === 0 ? "bg-danger" : "bg-primary");
+  };
+
+  return (
+    <>
+      <h1>
+        <span className={formatHeaderPhrase()}>
+          {users.length > 0
+            ? `${users.length + renderPhrse(users.length)} с тобой сегодня`
+            : "Никто с тобой не тусанёт"}
+        </span>
+      </h1>
+
+      {users.length !== 0 ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретились, раз</th>
+              <th scope="col">оценка</th>
+              <th scope="col">delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+
+                  <td>
+                    {user.qualities.map((qyalit) => (
+                      <span
+                        className={`badge text-bg-${qyalit.color} m-1`}
+                        key={qyalit._id}
+                      >
+                        {qyalit.name}
+                      </span>
+                    ))}
+                  </td>
+
+                  <td>{user.profession.name}</td>
+
+                  <td>{user.completedMeetings}</td>
+
+                  <td>{user.rate}</td>
+
+                  <td>
+                    <button
+                      onClick={() => handleDelete(user._id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
-
 export default Users;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import Users from "./component/users.jsx";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Users from "./component/users";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// const root = ReactDom.createRoot(document.getElementById("root"));
+// root.render(
+ReactDom.render(
   <React.StrictMode>
     <Users />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
